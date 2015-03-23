f250aa6d3b3263d3f168c158966769c7b2f759d4
Bart Aelterman <bart.aelterman@gmail.com>
2015-03-23 12:16:16 +0100

    close file connection

--- scripts/merge_tracking_data.R
+++ scripts/merge_tracking_data.R
@@ -7,3 +7,4 @@ outputfile = "fish_tracking.csv"
 mergedContent = merge_files(dir)
 f = file(outputfile, "w+") # open the existing file and truncate it
 write.csv(mergedContent, f, sep=",", eol="\n")
+close(f)
