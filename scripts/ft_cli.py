b960a8eac7e9423b251f247e1557bc0f9255eebf
bartaelterman <bart.aelterman@gmail.com>
2015-09-15 15:41:09 +0200

    fish argument capitalization

--- scripts/ft_cli.py
+++ scripts/ft_cli.py
@@ -13,15 +13,15 @@ def fish_tracking():
 @click.option('--minutes', default=60, help='maximum number of minutes in interval (default: 60)')
 @click.option('--st_mapping', default='./data/station_names.md', help='points to the station names mapping file. If given, old stations names will be replaced by new ones.')
 @click.option('--debug/--no-debug', default=False)
-def aggregate(DIRECTORY, minutes, st_mapping, debug):
+def aggregate(directory, minutes, st_mapping, debug):
     """Create aggregated detections file based on data in DIRECTORY"""
     agg = Aggregator()
     detection_dataframes = []
-    for fname in os.listdir(DIRECTORY):
+    for fname in os.listdir(directory):
         if fname.split('.')[-1] == 'csv':
             if debug:
                 print fname
-            tmpdetections = agg.parse_detections(os.path.join(DIRECTORY, fname), station_mapping=st_mapping)
+            tmpdetections = agg.parse_detections(os.path.join(directory, fname), station_mapping=st_mapping)
             detection_dataframes.append(tmpdetections)
     detections = pd.concat(detection_dataframes)
     intervals = agg.aggregate(detections, minutes_delta=minutes, time_format='iso')
@@ -31,15 +31,15 @@ def aggregate(DIRECTORY, minutes, st_mapping, debug):
 @click.argument('DIRECTORY', type=click.Path(exists=True))
 @click.option('--st_mapping', default='./data/station_names.csv', help='points to the station names mapping file. If given, old stations names will be replaced by new ones.')
 @click.option('--debug/--no-debug', default=False)
-def parse(DIRECTORY, st_mapping, debug):
+def parse(directory, st_mapping, debug):
     """Create consolidated detections file based on data in DIRECTORY but do not aggregate time frames"""
     agg = Aggregator()
     detection_dataframes = []
-    for fname in os.listdir(DIRECTORY):
+    for fname in os.listdir(directory):
         if fname.split('.')[-1] == 'csv':
             if debug:
                 print fname
-            tmpdetections = agg.parse_detections(os.path.join(DIRECTORY, fname), station_mapping=st_mapping)
+            tmpdetections = agg.parse_detections(os.path.join(directory, fname), station_mapping=st_mapping)
             detection_dataframes.append(tmpdetections)
     detections = pd.concat(detection_dataframes)
     print detections.to_csv(index=False)
