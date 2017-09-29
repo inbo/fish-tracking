3b9eb2483c1493ae4ac2ffd2b92d3fa392e465d5
stijnvanhoey <stijnvanhoey@gmail.com>
2017-09-29 12:02:44 +0200

    Add reading data to script

--- scripts/zerinfl_model/effect_variabelen.R
+++ scripts/zerinfl_model/effect_variabelen.R
@@ -1,6 +1,10 @@
-#final model
+# final model
 library(pscl)
-nb2=zeroinfl (count_max ~ WindSpeed_scaled +  WindDir_scaled + Tilt_scaled * degree_scaled + Noise_scaled + CurrentSpeed_scaled + CurrentDir_scaled+ offset(log(maxi)), 
+
+# Read scaled data set
+scaled <- read.csv("./scaled_data.csv")
+
+nb2=zeroinfl (count_max ~ WindSpeed_scaled +  WindDir_scaled + Tilt_scaled * degree_scaled + Noise_scaled + CurrentSpeed_scaled + CurrentDir_scaled+ offset(log(maxi)),
               data=scaled, dist = "negbin")
 summary (nb2)
 
@@ -30,7 +34,7 @@ newdata$maxi                <- median(scaled$maxi)
 
 summary(newdata)
 
-# Predict the expected values for each variable 
+# Predict the expected values for each variable
 #although we ask for se (se.fit=TRUE), we don't get them...
 P1<- predict(nb2,newdata=newdata,
              se.fit=TRUE,MC=2500)
@@ -52,13 +56,13 @@ lines(x=newdata$WindSpeed_scaled,
       y=newdata$P1,
       lwd=3)
 
-lines(x = newdata$CurrentSpeed_scaled, 
-      y = exp(newdata$P1 + 1.96 * P1$se.fit), 
-      lwd = 3, 
+lines(x = newdata$CurrentSpeed_scaled,
+      y = exp(newdata$P1 + 1.96 * P1$se.fit),
+      lwd = 3,
       lty = 2)
-lines(x = MyData$WindSpeed_scaled, 
-      y = exp(newdata$P1 - 1.96 * P1$se.fit), 
-      lwd = 3, 
+lines(x = MyData$WindSpeed_scaled,
+      y = exp(newdata$P1 - 1.96 * P1$se.fit),
+      lwd = 3,
       lty = 2)
 
 dev.copy(tiff,'Marginal_effect_WindSpeed.tiff')
