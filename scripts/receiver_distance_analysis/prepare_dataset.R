d93e06eb8a28eb239f47b8b43422b2b8b9558130
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2021-07-06 12:09:02 +0200

    calculate distances for projects stour and nene

--- scripts/receiver_distance_analysis/prepare_dataset.R
+++ scripts/receiver_distance_analysis/prepare_dataset.R
@@ -247,7 +247,7 @@ plot(superpolder)
 # SET STUDY AREA
 # -----------------------
 #study.area <- study.area  # When the LifeWatch network is taken into account; sea 'Combine the shape files'
-study.area <- frome
+study.area <- semp
 
 # ----------------
 # LOAD DETECTION STATION NETWORK
@@ -376,7 +376,7 @@ projections.locations.receivers <- find.projections.receivers(
 
 # for homogeneous study areas
 projections.locations.receivers <- find.projections.receivers(
-  shape.study.area = frome,
+  shape.study.area = semp,
   receivers = locations.receivers,
   projection = coordinate_epsg
 )
@@ -462,7 +462,7 @@ cst.dst.frame_corrected <- get.distance.matrix(
 # inspect distance output
 cst.dst.frame_corrected
 # save distances
-write.csv(cst.dst.frame_corrected, "./results/distancematrix_2014_frome.csv")
+write.csv(cst.dst.frame_corrected, "./results/distancematrix_2014_nene.csv")
 
 
 # IDEA ...
