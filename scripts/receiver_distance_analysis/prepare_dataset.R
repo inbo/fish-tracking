d011a777de2a43d2caae1ef753b1f132ffae744e
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2021-09-28 15:20:15 +0200

    calculate distances for albert canal network

--- scripts/receiver_distance_analysis/prepare_dataset.R
+++ scripts/receiver_distance_analysis/prepare_dataset.R
@@ -526,10 +526,10 @@ locations.receivers <- load.receivers(
 
 # for study area combined by two study areas made of polygons and lines 
 projections.locations.receivers <- find.projections.receivers(
-  shape.study.area = leopoldkanaal,
+  shape.study.area = albertkanaal_zeeschelde,
   receivers = locations.receivers,
   projection = coordinate_epsg,
-  shape.study.area2 = ws_bpns, 
+  shape.study.area2 = meuse, 
   shape.study.area_merged = study.area
 )
 
@@ -553,10 +553,10 @@ mapView(locations.receivers, col.regions = "red", map.types = "OpenStreetMap",
           label = projections.locations.receivers$station_name)
 
 # for study.area with mixed polygons and lines
-leaflet(leopoldkanaal %>% st_transform(crs = 4326)) %>%
+leaflet(albertkanaal_zeeschelde %>% st_transform(crs = 4326)) %>%
   addTiles(group = "OSM (default)") %>%
   addPolylines() %>%
-  addPolygons(data = ws_bpns %>% st_transform(4326)) %>%
+  addPolygons(data = meuse %>% st_transform(4326)) %>%
   addCircleMarkers(data = locations.receivers %>% st_transform(4326),
                    radius = 3,
                    color = "red",
@@ -590,8 +590,8 @@ study.area.binary <- shape.to.binarymask(
 
 # for a study area which is a combination of polygons and lines
 study.area.binary <- shape.to.binarymask(
-  shape.study.area = leopoldkanaal,
-  shape.study.area2 = ws_bpns,
+  shape.study.area = albertkanaal_zeeschelde,
+  shape.study.area2 = meuse,
   shape.study.area_merged = study.area,
   receivers = projections.locations.receivers,
   resolution = res)
@@ -621,7 +621,7 @@ cst.dst.frame_corrected <- get.distance.matrix(
 # inspect distance output
 cst.dst.frame_corrected
 # save distances
-write.csv(cst.dst.frame_corrected, "./results/distancematrix_noordzeekanaal.csv")
+write.csv(cst.dst.frame_corrected, "./results/distancematrix_2013_albertkanaal.csv")
 
 
 # IDEA ...
