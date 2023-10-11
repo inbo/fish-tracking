560a7e1a8c08f37c1572394dcfa90d12fd8aeffd
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2023-10-11 16:46:32 +0200

    update michimit network

--- scripts/receiver_distance_analysis/prepare_dataset.R
+++ scripts/receiver_distance_analysis/prepare_dataset.R
@@ -683,10 +683,10 @@ mapView(locations.receivers, col.regions = "red", map.types = "OpenStreetMap",
           label = projections.locations.receivers$station_name)
 
 # for study.area with mixed polygons and lines
-leaflet(river %>% st_transform(crs = 4326)) %>%
+leaflet(michimit %>% st_transform(crs = 4326)) %>%
   addTiles(group = "OSM (default)") %>%
   addPolylines() %>%
-  addPolygons(data = estuary %>% st_transform(4326)) %>%
+  addPolygons(data = ws_bpns %>% st_transform(4326)) %>%
   addCircleMarkers(data = locations.receivers %>% st_transform(4326),
                    radius = 3,
                    color = "red",
@@ -707,7 +707,7 @@ leaflet(river %>% st_transform(crs = 4326)) %>%
 # ------------------------
 # CONVERT SHAPE TO RASTER
 # ------------------------
-res <- 10 # pixel is a square:  res x res (in meters)
+res <- 100 # pixel is a square:  res x res (in meters)
 
 # First time running the following function can give an error that can be ignored. The code will provide the output anyway. See stackoverflow link for more info about the bug.
 #https://stackoverflow.com/questions/61598340/why-does-rastertopoints-generate-an-error-on-first-call-but-not-second
@@ -720,8 +720,8 @@ study.area.binary <- shape.to.binarymask(
 
 # for a study area which is a combination of polygons and lines
 study.area.binary <- shape.to.binarymask(
-  shape.study.area = river,
-  shape.study.area2 = estuary,
+  shape.study.area = michimit,
+  shape.study.area2 = ws_bpns,
   shape.study.area_merged = study.area,
   receivers = projections.locations.receivers,
   resolution = res)
@@ -751,7 +751,7 @@ cst.dst.frame_corrected <- get.distance.matrix(
 # inspect distance output
 cst.dst.frame_corrected
 # save distances
-write.csv(cst.dst.frame_corrected, "./results/distancematrix_shakimardan.csv")
+write.csv(cst.dst.frame_corrected, "./results/distancematrix_michimit_oct2023.csv")
 
 
 # IDEA ...
